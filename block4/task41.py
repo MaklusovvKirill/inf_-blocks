7d4e883f06043606b50824c040fea10e597e85a7
MaklusovvKirill <maklusovuskirillus@gmail.com>
2025-01-16 10:22:35 +0000

    a

---
+++ block4/task41.py
@@ -0,0 +1,13 @@
+import math
+
+
+def f(x):
+    if 0.2 <= x <= 0.9:
+        return math.sin(x)
+    else:
+        return 1
+
+
+x = float(input("Введите вещественное число x: "))
+result = f(x)
+print(f"f({x}) = {result}")
