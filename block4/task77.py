c78d2da70b081232873db55f560ab5d42bc9f67c
MaklusovvKirill <maklusovuskirillus@gmail.com>
2025-01-21 09:04:40 +0000

    a

---
+++ block4/task77.py
@@ -0,0 +1,11 @@
+import math
+
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число: "))
+
+
+if sqrt(second) < first:
+    second_number *= 5
+
+
+print(f"Второе число: {second}")
---
+++ block4/task85.py
@@ -0,0 +1,12 @@
+x = float(input("Введите значение x: "))
+
+
+if x < -1:
+    y = 1
+elif x > -1:
+    y = x
+else:
+    y = 1
+
+
+print(f"y({x}) = {y}")
