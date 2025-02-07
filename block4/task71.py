74dbd1e2021d5eb80f9d868c26ff9b54aeab646b
MaklusovvKirill <maklusovuskirillus@gmail.com>
2025-02-07 07:36:58 +0000

    a

--- block4/task71.py
+++ block4/task71.py
@@ -1,10 +1,12 @@
 a = int(input("Введите число: "))
 b = int(input("Введите число: "))
 c = int(input("Введите число: "))
-mx = a
-if b > mx:
-    mx = b
-else: c > mx
-mx =c
 
-print(mx)
+h = a
+
+
+if b > h:
+    h = b
+else:
+    h = c
+print(h)
